623e4d2b19ea2941832237e44c30420e9c32b610
juanpark <juanpark@me.com>
2025-03-03 21:38:07 +0900

    testing copying whole Java project folder into git

---
+++ curriculum/25.02.28/AppJava/src/com/sec01/DataType05.java
@@ -0,0 +1,17 @@
+package com.sec01;
+
+public class DataType05 {
+	public static void main(String[] args) {
+		float f = 3.141f;
+		double d = 3.141d;
+		double d2 = 3.141;
+		System.out.println(f);
+		System.out.println(d);
+		System.out.println(d2);
+		
+		
+
+	}
+}
+
+

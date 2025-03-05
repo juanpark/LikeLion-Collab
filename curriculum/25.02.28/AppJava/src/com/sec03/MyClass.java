74a79ca98d42265b3e6b393f905e486b18dcf7cd
juanpark <juanpark@me.com>
2025-03-05 15:55:05 +0900

    curriculum update upto 25.03.05 16:00

---
+++ curriculum/25.02.28/AppJava/src/com/sec03/MyClass.java
@@ -0,0 +1,27 @@
+package com.sec03;
+
+public class MyClass {
+	private int a;
+	
+	// constructor
+	public MyClass() {
+		// if not declared, default constructor is provided
+		System.out.println("I am default constructor " + this.a);
+	}
+	// overloading
+	public MyClass(int a ) {
+		this.a = a;
+		System.out.println("overload constructor " + this.a);
+	}
+	
+	
+	public static void main(String[] args) {
+
+		// MyClass m = new MyClass();
+		// System.out.println(m);
+		
+		// overloading
+		MyClass m1 = new MyClass(200);
+	}
+
+}

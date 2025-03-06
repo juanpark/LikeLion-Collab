54e8fd43a021266d6671ca8277c0407149ae6c9c
juanpark <juanpark@me.com>
2025-03-06 10:33:40 +0900

    curriculum update upto 25.03.06 10:30

--- curriculum/25.02.28/AppJava/src/com/sec02/ArithOperator.java
+++ curriculum/25.02.28/AppJava/src/com/sec02/ArithOperator.java
@@ -1,26 +1,38 @@
 package com.sec02;
 
+// 지역변수는 메소드안에 선언하는 변수
+// 멤버변수는 클래스안에 선언하는 변수
+// 클래스에서 선언할 때 static은 공유가 된다 
 public class ArithOperator {
-	public static void main(String[] args) {
-
-		int result = 0;
-		int value01 = 50;
-		int value02 = 20;
-
+	static int result = 0;
+	static int value01 = 50;
+	static int value02 = 20;
+	
+	public void prn() {
 		result = value01 + value02;
-		System.out.println(value01 + "+ " + value02 + " =" + result);
+		System.out.println(value01 + " + " + value02 + " = " + result);
 
 		result = value01 - value02;
-		System.out.println(value01 + "- " + value02 + " =" + result);
+		System.out.println(value01 + " - " + value02 + " = " + result);
 
 		result = value01 * value02;
-		System.out.println(value01 + "* " + value02 + " =" + result);
+		System.out.println(value01 + " * " + value02 + " = " + result);
 
 		result = value01 / value02;
-		System.out.println(value01 + "/ " + value02 + " =" + result);
+		System.out.println(value01 + " / " + value02 + " = " + result);
 
 		result = value01 % value02;
-		System.out.println(value01 + "% " + value02 + " =" + result);
+		System.out.println(value01 + " % " + value02 + " = " + result);
+	}
+	
+	public static void main(String[] args) {
+			
+			// getting temp address for .prn
+			new ArithOperator().prn();
+			System.out.println("===================");
+			new ArithOperator().prn();
+			System.out.println("===================");
+			System.out.println(ArithOperator.result);
 
 	}
 

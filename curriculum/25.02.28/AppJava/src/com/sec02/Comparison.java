d4f519f7163a761fe321785b43fd832430fa811c
juanpark <juanpark@me.com>
2025-03-06 13:42:28 +0900

    curriculum update upto 25.03.06 13:40

--- curriculum/25.02.28/AppJava/src/com/sec02/Comparison.java
+++ curriculum/25.02.28/AppJava/src/com/sec02/Comparison.java
@@ -1,18 +1,17 @@
 package com.sec02;
 
 public class Comparison {
-
-	@SuppressWarnings("unused")
+	
 	public static void main(String[] args) {
+		
+		int a = 10;
+		int b = 2;
+		
+		System.out.println( a > b );
+		System.out.println( a & b );  // bitwise operation 
+		System.out.println( (a > b) && (b == a) ); 
+		
 
-		int num, assign;
-		num = 10;
-
-		int a = 3;
-		int b = 4;
-
-		System.out.println((a > b++) && (a < b++));// false & true = false
-		System.out.println("a=" + a + "  b=" + b);
 
 	}
 

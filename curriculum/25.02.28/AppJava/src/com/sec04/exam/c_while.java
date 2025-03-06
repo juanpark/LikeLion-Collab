fb057400f9d2d98ba4d28ba5a848f87d7602abd5
juanpark <juanpark@me.com>
2025-03-06 15:41:15 +0900

    curriculum update upto 25.03.06 15:40

---
+++ curriculum/25.02.28/AppJava/src/com/sec04/exam/c_while.java
@@ -0,0 +1,17 @@
+package com.sec04.exam;
+
+public class c_while {
+
+	public static void main(String[] args) {
+		
+		int i = 0;
+		
+		while(i < 10) {	// 무한루프를 지정하게 되면 조건문을 지정해서 멈춘다. 또는 제어변수를 지정한다.
+			System.out.println("Command");
+			i++;
+		}
+			
+
+	}
+
+}

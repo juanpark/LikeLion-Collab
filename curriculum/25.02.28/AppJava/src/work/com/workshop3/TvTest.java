333d3fb60b4e4b3fec6babe6c62e006759be6f56
juanpark <juanpark@me.com>
2025-03-07 22:58:08 +0900

    curriculum update upto 25.03.07 22:58 workshop3

--- curriculum/25.02.28/AppJava/src/work/com/workshop3/TvTest.java
+++ curriculum/25.02.28/AppJava/src/work/com/workshop3/TvTest.java
@@ -1,84 +1,84 @@
 package work.com.workshop3;
 
-public class TvTest {
+
 	
-	public static class Tv {
-		private String name;
-		private int price;
-		private String descript;
+class Tv {
+	private String name;
+	private int price;
+	private String descript;
+	
+	public Tv() {
 		
-		public Tv() {
-			
-		};
-		
-		// Constructor
-		public Tv(String name, int price, String description) {
-			this.name = name;
-			this.price = price;
-			this.descript = descript;
-		}
-		
+	};
+	
+	// Constructor
+	public Tv(String name, int price, String description) {
+		this.name = name;
+		this.price = price;
+		this.descript = descript;
+	}
+	
 
 
-		/**
-		 * @return the name
-		 */
-		public String getName() {
-			return name;
-		}
-	
-	
-	
-		/**
-		 * @param name the name to set
-		 */
-		public void setName(String name) {
-			this.name = name;
-		}
-	
-	
-	
-		/**
-		 * @return the price
-		 */
-		public int getPrice() {
-			return price;
-		}
-	
-	
-	
-		/**
-		 * @param price the price to set
-		 */
-		public void setPrice(int price) {
-			this.price = price;
-		}
-	
-	
-	
-		/**
-		 * @return the descript
-		 */
-		public String getDescript() {
-			return descript;
-		}
-	
-	
-	
-		/**
-		 * @param descript the descript to set
-		 */
-		public void setDescript(String descript) {
-			this.descript = descript;
-		}
-		
-		@Override
-		public String toString() {
-			return String.format("%-10s %-10d %-10s", name, price, descript);
-		}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
 	}
 
 
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+	/**
+	 * @return the price
+	 */
+	public int getPrice() {
+		return price;
+	}
+
+
+
+	/**
+	 * @param price the price to set
+	 */
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+
+
+	/**
+	 * @return the descript
+	 */
+	public String getDescript() {
+		return descript;
+	}
+
+
+
+	/**
+	 * @param descript the descript to set
+	 */
+	public void setDescript(String descript) {
+		this.descript = descript;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("%-10s %-10d %-10s", name, price, descript);
+	}
+}
+
+public class TvTest{
 	public static void main(String[] args) {
 		Tv tv1 = new Tv();
 		Tv tv2 = new Tv();
@@ -102,21 +102,21 @@ public class TvTest {
 			System.out.println(tv);
 		}
 		
-		int maxPrice = tvs[0].price;
+		int maxPrice = tvs[0].getPrice();
 		for (int i = 0; i < tvs.length; i++) {
-			if (tvs[i].price > maxPrice) {
+			if (tvs[i].getPrice() > maxPrice) {
 				maxPrice = i;
 			}
 		}
-		System.out.println("가격이 가장 비싼 제품: " + tvs[maxPrice].name);
+		System.out.println("가격이 가장 비싼 제품: " + tvs[maxPrice].getName());
 		
-		int minPrice = tvs[0].price;
+		int minPrice = tvs[0].getPrice();
 		for (int i = 0; i < tvs.length; i++) {
-			if (tvs[i].price < minPrice) {
+			if (tvs[i].getPrice() < minPrice) {
 				minPrice = i;
 			}
 		}
-		System.out.println("가격이 가장 저렴한 제품: " + tvs[minPrice].name);
+		System.out.println("가격이 가장 저렴한 제품: " + tvs[minPrice].getName());
 	
 		
 		

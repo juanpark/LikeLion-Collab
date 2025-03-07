53b55d6f88ce5dcf572c1b8f68a1a9bf10517237
juanpark <juanpark@me.com>
2025-03-07 11:50:07 +0900

    curriculum update upto 25.03.07 11:50

--- curriculum/25.02.28/AppJava/src/com/sec04/WhileTest2.java
+++ curriculum/25.02.28/AppJava/src/com/sec04/WhileTest2.java
@@ -23,12 +23,59 @@ public class WhileTest2 {
 		
 	}
 	
+	/*
+	 * 1 ~ 100까지 숫자를 출력해보자 while로 작성하자 
+	 * 조건 1 : 짝수만 출력하
+	 */
+	private static void test03() {
+		int i = 1;	// 초기값
+		int cnt = 0;	// 갯수 출력 변
+		while (i <= 100) {
+			if (i % 2 == 0) {
+				System.out.printf("%5d ", i);
+				cnt++;
+			}
+			i++;
+		}
+		System.out.println(" end i = " + i + " | 짝수의 개수 cnt = " + cnt);
+	}
+	
+	/*
+	 * 1 ~ 100까지 숫자 출력 
+	 * 조건 1 : 5의 배수에는 하트 조건 
+	 * 2 : 5의 배수 줄바꿈 조건 
+	 * 3 : 하트의 갯수는?
+	 */
+	private static void test04() {
+		int i = 1;	// 초기값
+		int cnt = 0;	// 갯수 출력 변
+		while (i <= 100) {
+			if (i % 5 == 0) {
+				System.out.printf("%5c\n ", '❤');
+				cnt++;
+			} else {
+				System.out.printf("%5d", i);
+			}
+			i++;
+		}
+		System.out.println(" end i = " + i + " | 하트의 개수 cnt = " + cnt);
+		
+	}
+	
 	public static void main(String[] args) {
 		test01();
 		System.out.printf("\n======================================\n");
 		test02();
+		System.out.printf("\n======================================\n");
+		test03();
+		System.out.printf("\n======================================\n");
+		test04();
 
 	}// main end
+
+
+
+
 
 
 

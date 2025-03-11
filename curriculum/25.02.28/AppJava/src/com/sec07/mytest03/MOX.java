0e2666b5aedf03ac5ed487dbda6506436bf391d9
juanpark <juanpark@me.com>
2025-03-11 15:58:39 +0900

    curriculum update upto 25.03.11 15:50

--- curriculum/25.02.28/AppJava/src/com/sec07/mytest03/MOX.java
+++ curriculum/25.02.28/AppJava/src/com/sec07/mytest03/MOX.java
@@ -2,7 +2,8 @@ package com.sec07.mytest03;
 
 public class MOX {
 	public static void main(String[] args) {
-		ScoreOX p[] = { new ScoreOX(1, "루리", new int[] { 1, 2, 1, 3, 4 }),
+		ScoreOX p[] = { 
+				new ScoreOX(1, "루리", new int[] { 1, 2, 1, 3, 4 }),
 				new ScoreOX(2, "루세", new int[] { 1, 1, 1, 1, 1 }),
 				new ScoreOX(3, "루오", new int[] { 1, 2, 1, 3, 1 }),
 				new ScoreOX(4, "폴리", new int[] { 1, 1, 1, 3, 1 }),
@@ -11,16 +12,16 @@ public class MOX {
 		for (int i = 0; i < 5; i++) {
 			p[i].compute();
 		}
-		ScoreOX.Ranking(p);
+		ScoreOX.Ranking(p);  // 공용메소드 static
 
 		System.out.println(" * * 시험결과 * * \n");
-		System.out.println("------------------------------------------");
-		System.out.println("번호  이름   1   2   3   4   5      점수   등수 ");
-		System.out.println("------------------------------------------");
+		System.out.println("---------------------------------------------------");
+		System.out.println("번호    이름    1    2    3    4    5     점수    등수 ");
+		System.out.println("---------------------------------------------------");
 
 		for (int i = 0; i < 5; i++)
 			p[i].display();
-		System.out.println("\n 전체맞은 개수  = " + ScoreOX.tot);
+		System.out.println("\n 전체맞은 개수  = " + ScoreOX.tot);  // 공용변수 static
 
 	}
 }
--- curriculum/25.02.28/AppJava/src/com/sec07/mytest03/StaticTest.java
+++ curriculum/25.02.28/AppJava/src/com/sec07/mytest03/StaticTest.java
@@ -2,16 +2,31 @@ package com.sec07.mytest03;
 
 public class StaticTest {	
 	public static int static_var = 0;
-
+	private int non_static;
+	
 	public StaticTest() {
 		static_var++;
+		// non_static++;
 	}
+	
+	/*public int getNon_static() {
+		return non_static;
+	}*/
 
 	public static void main(String[] args) {
 		StaticTest st1 = new StaticTest();
 		StaticTest st2 = new StaticTest();
 		StaticTest st3 = new StaticTest();
 		StaticTest st4 = new StaticTest();
+		
+		for (int i = 0; i < 5; i++) {
+			new StaticTest();
+		}
 		System.out.println(StaticTest.static_var);
+		
+		/*for (int i = 0; i < 5; i++) {
+			System.out.println(new StaticTest().getNon_static());*/
+		}
+
 	}
-}
+

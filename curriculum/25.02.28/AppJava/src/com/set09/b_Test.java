e89890ff9de335573007d957b0393dd7ee25662d
juanpark <juanpark@me.com>
2025-03-12 15:56:29 +0900

    curriculum update upto 25.03.12 15:55

--- curriculum/25.02.28/AppJava/src/com/set09/b_Test.java
+++ curriculum/25.02.28/AppJava/src/com/set09/b_Test.java
@@ -1,5 +1,28 @@
 package com.set09;
 
-public class b_Test {
+import java.io.BufferedReader;
 
+public class b_Test {
+	
+	public static void prn() throws ArithmeticException{
+		
+		throw new ArithmeticException(); // 명시로 예외 발생 
+		
+	}
+	
+	public static void prn01() throws ArithmeticException{
+		prn();
+	}
+	
+	public static void prn02() throws ArithmeticException{
+		prn01();
+	}
+
+	public static void main(String[] args) {
+		try {
+			prn02();
+		} catch (ArithmeticException e) {
+			
+		}
+	}
 }

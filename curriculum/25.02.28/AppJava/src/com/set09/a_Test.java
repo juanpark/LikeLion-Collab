9269be3510d0d6082a36a877ad696d763c8a4a14
juanpark <juanpark@me.com>
2025-03-12 15:56:29 +0900

    curriculum update upto 25.03.12 15:55

--- curriculum/25.02.28/AppJava/src/com/set09/a_Test.java
+++ curriculum/25.02.28/AppJava/src/com/set09/a_Test.java
@@ -4,14 +4,21 @@ public class a_Test {
 
 	public static void main(String[] args) {
 		int a = 100;
-		int b = 0;
+		int b = 5;
 		int res = 0;
 		
 		try {
 		res = a / b;
 		// ArithmethicException AE = new java.lang.ArtithmeticException("/ by zero")
 		} catch (ArithmeticException AE) { 
+			
 			System.out.println("Exception: " + AE.getMessage());
+		} catch (RuntimeException RE) {
+			System.out.println("RuntimeException");
+		} catch (Exception E) {
+			System.out.println("Other Exception");
+		} finally {
+			System.out.println("Required at End");
 		}
 		System.out.printf("a = %5d  b=%5d  res=%5d \n", a, b, res);
 		
--- curriculum/25.02.28/AppJava/src/com/set09/c_Test.java
+++ curriculum/25.02.28/AppJava/src/com/set09/c_Test.java
@@ -1,5 +1,21 @@
 package com.set09;
+import java.util.Scanner;
 
 public class c_Test {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int r = 0;
+		
+		try {
+			r = sc.nextInt();
+		} catch (java.util.InputMismatchException IE) {
+			r = 0;
+			System.out.println("Exception: " + IE.getMessage());
+		}
+		System.out.println("r = " + r);
+		
+	}
 
 }

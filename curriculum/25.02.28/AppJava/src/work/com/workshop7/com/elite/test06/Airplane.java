cd9054426e776f285ce1a60ad3442f0941daf0f5
juanpark <juanpark@me.com>
2025-03-12 20:31:08 +0900

    curriculum update upto 25.03.12 20:30 Workshop7-7

---
+++ curriculum/25.02.28/AppJava/src/work/com/workshop7/com/elite/test06/Airplane.java
@@ -0,0 +1,21 @@
+package work.com.workshop7.com.elite.test06;
+
+public class Airplane extends Plane {
+
+	public Airplane() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Airplane(String planeName, int fuelSize) {
+		super(planeName, fuelSize);
+		// TODO Auto-generated constructor stub
+	}
+	
+	/*
+	 * @Override public void flight(int distance) { this.setFuelSize(distance);
+	 * this.getFuelSize() - 30;
+	 * 
+	 * distance 100 / 10 = 10 * 30 }
+	 */
+
+}
---
+++ curriculum/25.02.28/AppJava/src/work/com/workshop7/com/elite/test07/Bonus.java
@@ -0,0 +1,7 @@
+package work.com.workshop7.com.elite.test07;
+
+public interface Bonus {
+
+	// no body
+	void incentive(int pay);
+}

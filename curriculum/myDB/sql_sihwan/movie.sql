28f6e501c5ed9cea0359477aeffa7c025d009f25
juanpark <juanpark@me.com>
2025-03-21 17:04:31 +0900

    curriculum update upto 25.03.21 17:00 시환님 발표

---
+++ curriculum/myDB/sql_sihwan/movie.sql
@@ -0,0 +1,34 @@
+USE MY_P;
+CREATE TABLE 2020_movie_report (id INTEGER PRIMARY KEY, name TEXT, category TEXT, main_month INTEGER, view_count INTEGER);
+INSERT INTO 2020_movie_report 
+VALUES (1,'태극권 배우기', 'Action', 3, 120000),
+(2, '여름 왕국', 'Fantasy', 6, 10000000),
+(3, '성태후 일병 구하기', 'Drama', 5, 5000000),
+(4, '그날 너를 보았다', 'Romance', 4, 7000000),
+(5, '다이이지', 'Action', 5, 2300000),
+(6, '쓰리캅스', 'Action', 5, 7000000),
+(7, '매드미니멈', 'Action', 4, 8500000),
+(8, '무인도, 20일', 'Thriller', 4, 1000000),
+(9, '허씨표류기', 'Comedy', 5, 300000),
+(10, '링거전쟁', 'Thriller', 3, 150000),
+(11, '타임백투유', 'Romance', 3, 5000000),
+(12, '그렇게 살아간다', 'Drama', 3, 3200000),
+(13, '배틀스타', 'SF', 4, 1650000),
+(14, '내일은 결혼식', 'Comedy', 5, 2000000),
+(15, '화성탈출', 'SF', 5, 4500000);
+
+SELECT * FROM 2020_movie_report;
+
+/*
+Q2.
+현재, 2020_movie_report 테이블에서 영화별 장르(category), 주 상영 월(main_month), 
+그리고 총 관객 수(view_count)를 관리하고 있습니다. 
+다음과 같은 조건을 만족하는 데이터를 추출하세요.
+
+category 컬럼, main_month 컬럼 기준 그룹화
+그룹 중에서 주 상영 월이 5월이고, view_count의 합이 3000000(삼백만) 이상인 그룹만 선별해 주세요.
+category 컬럼, main_month 컬럼, 각 그룹에 속한 영화의 개수, 각 그룹의 관객 수 합계 출력
+각 그룹에 속한 영화의 개수에 movie_count라는 별칭을 붙여 주세요.
+각 그룹의 관객 수 합계에 total_audience라는 별칭을 붙여 주세요.
+*/
+*/
---
+++ curriculum/myDB/sql_sihwan/pizza.sql
@@ -0,0 +1,29 @@
+CREATE TABLE pizza_price_cost (id INTEGER PRIMARY KEY, name TEXT, price INTEGER, cost INTEGER);
+INSERT INTO pizza_price_cost VALUES (1, 'Potato Bacon Pizza', 22000, 14000);
+INSERT INTO pizza_price_cost VALUES (2, 'Sweet Potato Pizza', 24000, 14000);
+INSERT INTO pizza_price_cost VALUES (3, 'Combination Pizza', 25000, 13000);
+INSERT INTO pizza_price_cost VALUES (4, 'Bacon Cheddar Pizza', 32000, 20000);
+INSERT INTO pizza_price_cost VALUES (5, 'Pineapple Pizza', 25000, 22000);
+INSERT INTO pizza_price_cost VALUES (6, 'Garlic Shrimp Pizza', 26000, 19000);
+INSERT INTO pizza_price_cost VALUES (7, 'Cheeze Pizza', 23000, 17000);
+INSERT INTO pizza_price_cost VALUES (8, 'Pepperoni Pizza', 24000, 13000);
+
+SELECT * FROM pizza_price_cost;
+
+/*
+Q1. 
+pizza_price_cost 테이블에는 피자의 가격(price)과 원가(cost)가 저장되어 있습니다. 
+우리는 이 데이터를 활용하여 각 피자의 원가 대비 가격 비율을 분석하고, 효율성 기준으로 카테고리를 나누고자 합니다.
+이를 통해 비용 대비 가격이 낮은 메뉴를 파악하고, 원가 절감 방안을 찾거나 가격을 조정하는 전략을 수립할 수 있습니다.
+
+피자 이름(name) 컬럼
+
+가격(price) 컬럼
+
+효율성 컬럼-원가 대비 가격(price를 cost로 나눈 값)을 구해 주세요.-별칭은 efficiency로 설정해 주세요.
+효율성 기준 카테고리 구분 컬럼
+efficiency 값에 따라 아래와 같이 구분된 값을 넣어 주세요.
+   1 이상 1.5 미만: 저효율 메뉴1.5 이상 1.7 미만: 중효율 메뉴1.7 이상: 고효율 메뉴
+별칭은 efficiency_category로 설정해 주세요.
+이때 전체 로우를 efficiency 컬럼 기준, 그 다음 price 컬럼 순서대로 오름차순 정렬해 주세요. 여기서 위에서 5번째 로우까지만 출력합니다.
+*/

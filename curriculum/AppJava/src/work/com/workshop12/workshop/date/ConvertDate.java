26ae4e62a79c45aa1272891b47daaa6442502cb7
juanpark <juanpark@me.com>
2025-03-29 16:23:07 +0900

    curriculum update upto 25.03.29 Workshop12_Java

--- curriculum/AppJava/src/work/com/workshop12/workshop/date/ConvertDate.java
+++ curriculum/AppJava/src/work/com/workshop12/workshop/date/ConvertDate.java
@@ -1,5 +1,22 @@
 package work.com.workshop12.workshop.date;
 
-public class ConvertDate {
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
+public class ConvertDate {
+	
+	// Using Locale.Korean option to solve language issue
+	public String convert (Date date, int type) {
+		
+		String result = switch (type) {
+			case 1 -> new SimpleDateFormat("yyyy-MM-dd").format(date);
+			case 2 -> new SimpleDateFormat("yy년 M월 d일 EEEE", Locale.KOREAN).format(date);
+			case 3 -> new SimpleDateFormat("yyyy-MM-dd H:m:s a", Locale.KOREAN).format(date);
+			default -> "선택할 수 있는 타입이 아닙니다";
+		};
+		
+		return result;
+		
+	}
 }
---
+++ curriculum/AppJava/src/work/com/workshop12/workshop/string2/ConvertString.java
@@ -0,0 +1,9 @@
+package work.com.workshop12.workshop.string2;
+
+public class ConvertString {
+
+		public String convert (int money) {
+			
+		return String.format("%,d", money);
+	}
+}

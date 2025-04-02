61153ea689da5b629429aacb69f298b62427b13d
juanpark <juanpark@me.com>
2025-04-02 10:15:37 +0900

    curriculum update upto 25.04.02 10:15

--- curriculum/AppJava/myDB/sql/n_procedure.sql
+++ curriculum/AppJava/myDB/sql/n_procedure.sql
@@ -60,27 +60,84 @@ FROM EMP;
 
 DROP TABLE EMP_TEST;
 -- Q1) 제약 조건이 없는 사원 테이블 `EMP_TEST`를 생성해보자.
-CREATE EMP_TEST
+CREATE TABLE EMP_TEST
+AS
+SELECT *
+FROM EMP;
+
+SELECT *
+FROM EMP_TEST;
 
 -- Q2) `PRO01_INSERT`() 프로시저를 작성하여, `EMP_TEST`에 사원의 번호, 이름, 부서번호를 여러 건 입력하자.
 
+INSERT INTO EMP_TEST (EMPNO, ENAME, DEPTNO)
+	VALUES(1, '1111', 10), (2, '2222', 20), (3, '3333', 30);
+
+CALL PRO01_INSERT();
+
+SELECT * FROM EMP_TEST;
+
 -- Q3) `PRO02_DELETE`(IN E_NO INT) 프로시저를 작성하여, 사원 번호를 입력받아 해당 사원을 삭제해보자.
+-- DELETE FROM EMP_TEST WHERE EMPNO = 1;
+SET SQL_SAFE_UPDATES = 0;
+CALL `PRO02_DELETE`(1);
+SET SQL_SAFE_UPDATES = 1;
+SELECT * FROM EMP_TEST;
+
 
 -- Q4) `PRO03_UPDATE`(IN E_NO INT, IN E_NAME VARCHAR(50)) 프로시저를 작성하여, 사번에 해당하는 사원의 이름을 수정하자.
+UPDATE EMP_TEST
+SET ENAME = E_NAME
+WHERE EMPNO = E_NO;
+
+SET SQL_SAFE_UPDATES = 0;
+CALL `PRO03_UPDATE`(2, '홍길동');
+SET SQL_SAFE_UPDATES = 1;
+SELECT * FROM EMP_TEST;
 
 -- Q5) `PRO04_SELECT`(IN EMP_NAME VARCHAR(50)) 프로시저를 작성하여, 사원 이름으로 해당 사원의 정보를 조회하자.
+SET @NAME = 'KING';
+SELECT *
+FROM EMP_TEST
+WHERE ENAME = EMP_NAME;
+
+CALL PRO04_SELECT(@NAME);
+
+-- 5-1 프로시저 전체 확인 해보자:
+SHOW PROCEDURE STATUS WHERE DB='MY_EMP';
+
+-- 5-2 특정 프로시저 내용을 확인 해보자:
+SHOW CREATE PROCEDURE MY_EMP.PRO01_INSERT;
 
 -- Q6) `PRO05_COUNT`(OUT CNT INT) 프로시저를 작성하여, EMP_TEST 테이블의 총 사원 수를 반환하자.
+SELECT COUNT(*) AS '총 사원 수'
+FROM EMP_TEST;
+
+CALL PRO05_COUNT(@RES); -- 프로시저 호출 시 OUT으로 리턴되는 값을 받을 변수를 선언
+SELECT @RES AS '총 사원 수'; -- 개수 출력
 
 -- Q7) `PRO06_SELECTALL`() 프로시저를 작성하여, EMP_TEST 테이블의 모든 데이터를 조회하자.
+SELECT * FROM EMP_TEST;
+
+CALL PRO06_SELECTALL();
 
 -- Q8) `PRO07_IF`(IN ENO INT) 프로시저를 작성하여,
 --      사원이 존재하면 해당 사원의 월급을 2배로 인상하고, 
 --      존재하지 않으면 메시지를 출력하자.
+SET @ENO = 7934;
+CALL PRO07_IF(@ENO);
+
+SET SQL_SAFE_UPDATES = 0;
+SET SQL_SAFE_UPDATES = 1;
+
+ROLLBACK;
+
 
 -- Q9) `PRO08_IF`(IN IN_SAL DECIMAL) 프로시저를 작성하여,
 --      입력받은 월급보다 많이 받는 사원의 이름과 급여를 출력하되, 
 --      존재하지 않으면 메시지를 출력하자.
+CALL PRO08_IF(5000);
+
 
 -- Q10) 모든 사원의 급여를 입력받은 비율만큼 인상하는 프로시저를 작성하자. (예: 비율 1.1 → 10% 인상)
 

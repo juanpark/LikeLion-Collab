46dddb1fa1aacc8b1819ef23b4810df4e4e171e0
juanpark <juanpark@me.com>
2025-04-03 17:13:19 +0900

    curriculum update upto 25.04.03 17:15 Workshop13-7

--- curriculum/AppJava/src/work/com/workshop13/com/mobile/Mobile.java
+++ curriculum/AppJava/src/work/com/workshop13/com/mobile/Mobile.java
@@ -14,9 +14,27 @@ public class Mobile {
 	}
 	
 	public String printInfo() {
-		return "NULL";
+		return String.format("%6s %-15s %.1f", code, name, price);
 	}
 	
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
 	
 }
 

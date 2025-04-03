adbe69b073a794c9126e9a29fd4be939d53c8bba
juanpark <juanpark@me.com>
2025-04-03 16:17:52 +0900

    curriculum update upto 25.04.03 16:00

---
+++ curriculum/AppJava/src/work/com/workshop13/com/mobile/MapTest.java
@@ -0,0 +1,23 @@
+package work.com.workshop13.com.mobile;
+
+public class MapTest {
+
+	public static void main(String[] args) {
+		
+		Map<String, Mobile> map = new HashMap<String, Mobile>();
+		double sum = 0.0;
+		// 4개의 Mobile 객체 생성
+		// map에 4개의 객체를 code를 키 값으로 넣는다.
+		// Mobile 정보를 출력 하고 합계를 계산
+		for(){
+		}
+		// Mobile 객체의 가격 정보를 10% 증가 시킨다.
+		for(){
+		}
+		for(){
+		}
+		// 변경된 정보를 출력 하고 합계를 계산
+
+	}
+
+}
---
+++ curriculum/AppJava/src/work/com/workshop13/com/mobile/Mobile.java
@@ -0,0 +1,22 @@
+package work.com.workshop13.com.mobile;
+
+public class Mobile {
+	private String	code;
+	private String	name;
+	private double	price;
+	public Mobile() {
+		
+	}
+	public Mobile(String code, String name, double price) {
+		this.code = code;
+		this.name = name;
+		this.price = price;
+	}
+	
+	public String printInfo() {
+		return "NULL";
+	}
+	
+	
+}
+

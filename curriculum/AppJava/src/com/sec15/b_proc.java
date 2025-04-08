4a92c13a2947389127d4c90d19489c6ff16691c5
juanpark <juanpark@me.com>
2025-04-08 11:42:15 +0900

    curriculum update upto 25.04.08 11:40

--- curriculum/AppJava/src/com/sec15/b_proc.java
+++ curriculum/AppJava/src/com/sec15/b_proc.java
@@ -1,28 +1,35 @@
 package com.sec15;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Scanner;
 import java.sql.CallableStatement;
 
 import static common.JDBCTemplate.*; //자주 사용하는 정적 메서드를 클래스명 없이 참조해서 바로 호출!
 
-public class a_proc {
+public class b_proc {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("삭제할 사원번호 : ");
+		int empno = sc.nextInt();
+		
 		// 연결확인 
 		Connection conn = getConnection();
 		CallableStatement cstmt = null;
 		
 		try {
-			String sql = "{ call PRO01_INSERT() }";
-			cstmt = conn.prepareCall(sql);
+			String sql = "{ call PRO02_DELETE(?) }";
+			cstmt = conn.prepareCall(sql); // 구문 준비
+			cstmt.setInt(1, empno); // 바인딩 
 			
-			cstmt.execute(); // 프로시저 실행
+			cstmt.executeUpdate(); // 프로시저 실행
+			System.out.println("삭제 했어!");
 			commit(conn);
 			
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			rollback(conn);
 			e.printStackTrace();
 		} finally {
+			sc.close();
 			Close(cstmt);
 			Close(conn);		
 			}
--- curriculum/AppJava/src/common/JDBCTemplate.java
+++ curriculum/AppJava/src/common/JDBCTemplate.java
@@ -6,7 +6,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-
+// JDBCTemplate 클래스틑 DB연결 생성, 자원 해제(Connection, Statement, ResultSet), 트랜젝션 처리(commit, rollback)
+// 을 일관되게 수행하는 유틸리티 클래스
 public class JDBCTemplate {
 
 	// DB 연결 하는 곳
@@ -14,11 +15,15 @@ public class JDBCTemplate {
 	Connection conn = null;
 	try {
 		
-		String url = "jdbc:mysql://127.0.0.1:3306/my_emp?";
+		String url = "jdbc:mysql://127.0.0.1:3306/my_emp";
 		String user = "mydb";
 		String password = "admin1234";
 		
 	    conn = DriverManager.getConnection(url, user, password);
+	    
+	    conn.setAutoCommit(false); // 트랜젝션 수동 설정 
+	    
+	    
 
 	    if(!conn.isClosed()) {
 	    	System.out.println("연결 중이야");
@@ -71,4 +76,31 @@ public class JDBCTemplate {
 			} // try - catch end
 		} // if end
 	} // Close() method end
+	
+	// 4. 트랜젝션 처리 (commit, rollback)
+	// 4-1 commit
+	public static void commit(Connection con) {
+		
+		if(con != null) {
+			try {
+				con.commit();
+			} catch (SQLException e) {
+				// e.printStackTrace();
+				System.out.println("데이터베이스 커밋 오류 :" + e.getMessage() + "\t" + e.getMessage());
+			} // try - catch end
+		} // if end
+	} // Close() method end
+	
+	// 4-2 rollback
+		public static void rollback(Connection con) {
+			
+			if(con != null) {
+				try {
+					con.rollback();
+				} catch (SQLException e) {
+					// e.printStackTrace();
+					System.out.println("데이터베이스 롤백 오류 :" + e.getMessage() + "\t" + e.getMessage());
+				} // try - catch end
+			} // if end
+		} // Close() method end
 }

bce752239fb57e3b89f52de9b2b6876ac4d8d008
juanpark <juanpark@me.com>
2025-04-16 14:28:41 +0900

    curriculum update upto 25.04.16 14:25

---
+++ curriculum/AppJava/src/com/sec17/intro/a_ZipDecompression2.java
@@ -0,0 +1,29 @@
+package com.sec17.intro;
+//압축된 .zip → 다시 읽기 (복원) 
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.zip.*;
+
+public class a_ZipDecompression2 {
+    public static void main(String[] args) {
+        File gzipFile = new File("output.txt.zip");
+
+        try (FileInputStream fis = new FileInputStream(gzipFile);
+             ZipInputStream zipIn = new ZipInputStream(fis);
+             InputStreamReader reader = new InputStreamReader(zipIn, StandardCharsets.UTF_8);
+             BufferedReader br = new BufferedReader(reader)) {
+        	
+        	 ZipEntry entry = zipIn.getNextEntry();
+        	
+            System.out.println(" 압축 해제된 내용:");
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+

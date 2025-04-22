9d44893a90bfb95b90c871e49ff2ed44365ecc53
juanpark <juanpark@me.com>
2025-04-22 19:29:50 +0900

    curriculum update upto 25.04.22 project chatserver

--- projects/chatserver/src/main/java/com/example/chatserver/config/WebSocketConfig.java
+++ projects/chatserver/src/main/java/com/example/chatserver/config/WebSocketConfig.java
@@ -10,18 +10,18 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        // Client will connect here
+        // 클라이언트 연결
         registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("*") // allow all origins (for dev)
-                .withSockJS(); // fallback for browsers that don't support native WebSocket
+                .setAllowedOriginPatterns("*") // 모든 origins 허가 (for dev)
+                .withSockJS(); // 웹소켓 지원 안하는 브라우저 대비
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        // Enable simple in-memory broker for messages with /topic prefix
+        // 클라이언트가 /topic/무언가로 메시지를 받을 수 있게 서버 안에 작은 메시지 전달 기능을 켜는 것
         registry.enableSimpleBroker("/topic");
 
-        // All messages sent from client must start with /app
+        // 클라이언트가 보내는 모든 메세지는 /app으로 시작
         registry.setApplicationDestinationPrefixes("/app");
     }
 }
--- projects/chatserver/src/main/java/com/example/chatserver/controller/ChatController.java
+++ projects/chatserver/src/main/java/com/example/chatserver/controller/ChatController.java
@@ -8,8 +8,8 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class ChatController {
 
-    @MessageMapping("/chat.sendMessage")    // Client sends to /app/chat.sendMessage
-    @SendTo("/topic/public")                // Server broadcasts to /topic/public
+    @MessageMapping("/chat.sendMessage")    // 클라이언트 발송 /app/chat.sendMessage
+    @SendTo("/topic/public")                // 서버 방송 /topic/public
     public ChatMessage sendMessage(ChatMessage message) {
         return message; // just echoing the message back for now
     }

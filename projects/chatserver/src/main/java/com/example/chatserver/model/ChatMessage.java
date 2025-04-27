e57092e0009db06502da7c90746833630a4df395
juanpark <juanpark@me.com>
2025-04-28 01:52:39 +0900

    Project 2025/04/28 챗서버 및 디비서버 테스트

--- projects/chatserver/src/main/java/com/example/chatserver/model/ChatMessage.java
+++ projects/chatserver/src/main/java/com/example/chatserver/model/ChatMessage.java
@@ -1,16 +1,31 @@
 package com.example.chatserver.model;
 
+import java.lang.annotation.Inherited;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class ChatMessage {
+
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String sender;
     private String content;
+    private String timestamp;
 
     // Constructors
     public ChatMessage() {
     }
 
-    public ChatMessage(String sender, String content) {
+    public ChatMessage(String sender, String content, String timestamp) {
         this.sender = sender;
         this.content = content;
+        this.timestamp = timestamp;
     }
 
     // Getters and setters
@@ -29,4 +44,14 @@ public class ChatMessage {
     public void setContent(String content) {
         this.content = content;
     }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    
 }

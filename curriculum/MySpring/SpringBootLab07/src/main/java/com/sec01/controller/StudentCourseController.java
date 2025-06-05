24cb1dd7b935770ad223857f03670a1559485481
juanpark <juanpark@me.com>
2025-06-05 10:26:15 +0900

    curriculum update upto 25.06.05 SpringBootLab07

--- curriculum/MySpring/SpringBootLab07/src/main/java/com/sec01/controller/StudentCourseController.java
+++ curriculum/MySpring/SpringBootLab07/src/main/java/com/sec01/controller/StudentCourseController.java
@@ -6,9 +6,6 @@ import com.sec01.entity.Course;
 import com.sec01.service.StudentCourseService;
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;

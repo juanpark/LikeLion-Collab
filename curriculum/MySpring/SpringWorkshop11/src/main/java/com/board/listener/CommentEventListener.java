dc9c1fed01b2c83335bb9b5933ada948f94ae64b
juanpark <juanpark@me.com>
2025-06-09 21:09:37 +0900

    curriculum update upto 25.06.09 SpeingWorkShop11 Problem1~3

--- curriculum/MySpring/SpringWorkshop11/src/main/java/com/board/listener/CommentEventListener.java
+++ curriculum/MySpring/SpringWorkshop11/src/main/java/com/board/listener/CommentEventListener.java
@@ -1,6 +1,7 @@
 package com.board.listener;
 
 import com.board.event.CommentCreatedEvent;
+import com.board.event.PostDeletedEvent;
 import com.board.repository.CommentRepository;
 
 import org.springframework.scheduling.annotation.Async;
@@ -25,5 +26,11 @@ public class CommentEventListener {
             Thread.currentThread().interrupt();
         }
         System.out.println("댓글 이벤트 처리 중... by " + event.getWriter());
+    }
+    
+    @EventListener
+    public void onPostDeleted(PostDeletedEvent event) {
+        System.out.println("해당 게시글의 댓글 삭제 실행: postId = " + event.getPostId());
+        commentRepository.deleteByPostId(event.getPostId());
     }
 }
